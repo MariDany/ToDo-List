fe8d1463191693ae60722558bf380e2f241e4ded
Maria Atencio <maridany0597@gmail.com>
2021-04-15 20:11:08 -0400

    Complete ToDO list project using node, express and EJS

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
+
+
+const app = express();
+
+let newItems = [];
+let workItems = [];
+let groceryItems = [];
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+   let day = date();
+    res.render('list', {listTitle: day, newListItems: newItems});
+})
+
+app.post("/", function(req,res){
+    let item = req.body.newItem;
+
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else if(req.body.list === "Grocery"){
+        groceryItems.push(item);
+        res.redirect("/groceries");
+    }
+    else{
+        newItems.push(item);
+        res.redirect("/");
+    }
+})
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle:"Work List",newListItems: workItems})
+})
+
+app.post("/work", function(req,res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+app.get("/groceries", function(req,res){
+    res.render("list", {listTitle: "Grocery List", newListItems: groceryItems})
+})
+
+app.post("/groceries", function(req,res){
+    let item = req.body.newItem;
+    groceryItems.push(item);
+    res.redirect("/groceries");
+})
+app.get("/about", function(req,res){
+    res.render("about");
+})
+
+app.listen(3000, function(){
+    console.log("This server is running on port 3000");
+})
